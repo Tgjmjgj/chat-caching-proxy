e1f96f32bcddea07c6fc3a45110dbd83ae899097
Pavel <Chesh397@mail.ru>
2020-08-25 16:10:05 +0300

    Cache every request by url only (without looking on headers)

--- server.js
+++ server.js
@@ -2,29 +2,40 @@ const express = require('express');
 const http  = require('http')
 const redis = require('redis');
 const requestProxy = require('express-request-proxy');
-
-require('redis-streams')(redis);
-const redisClient = redis.createClient();
+const proxy = require('express-http-proxy');
+const apicache = require('apicache');
 
 const app = express();
 
-app.all(
-  '/clientweb/api/v1/*',
-  requestProxy({
-    cache: redisClient,
-    cacheMaxAge: 300,
-    url: 'https://ocean08.brightpattern.com/clientweb/api/v1/*',
-  }),
-);
+// app.all(
+//   '/clientweb/api/v1/*',
+//   requestProxy({
+//     cacheMaxAge: 300,
+//     url: 'https://ocean08.brightpattern.com/clientweb/api/v1/*',
+//   }),
+// );
 
-app.get(
-    '/*',
-    requestProxy({
-        cache: redisClient,
-        cacheMaxAge: 300,
-        url: 'https://surflychat.web.app/*',
-    }),
-);
+// app.get(
+//     '/*',
+//     requestProxy({
+//         cacheMaxAge: 300,
+//         url: 'https://surflychat.web.app/*',
+//     }),
+// );
+
+const cache = apicache.middleware;
+apicache.options({
+    debug: true,
+    appendKey: (req, res) => req.protocol + '://' + req.get('host') + req.originalUrl,
+});
+
+app.use(cache('5 minutes'));
+
+app.use('/', proxy('https://ocean08.brightpattern.com/', {
+    filter: (req, res) => req.originalUrl.startsWith('/clientweb/api/v1/'),
+}));
+
+app.use('/', proxy('https://surflychat.web.app/'));
 
 http.createServer({}, app)
 .listen(3210, () => console.log('Server started at http://localhost:3210/'));
